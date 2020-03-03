a55d2a7f3e2c13edd2121b1158ecf95fd22c7130
Diewind <1290334299@qq.com>
2020-03-03 11:28:45 +0800

    修改已登录时显示页面

--- src/cookiedemo/login.jsx
+++ src/cookiedemo/login.jsx
@@ -4,6 +4,17 @@ import {Link} from 'react-router-dom'
     constructor(props){
         super(props);
     }
+    // 封装获取token的方法
+    getcookie = (keys) => {
+        let arr = document.cookie.split(";");
+    　　for(let i=0; i<arr.length; i++){
+            let carr=arr[i].split("=");
+    　　　　if(carr[0].trim() === keys){
+    　　　　    return carr[i];
+    　　　　}
+    　　}
+    　　return false;
+    }
     // 方法一：this.props.history.push跳页面参数还在
     // gotoIndex = () => {
     //     this.props.history.push('/',{token:"12345678"})   
@@ -11,6 +22,10 @@ import {Link} from 'react-router-dom'
     // 方法二：不会有bug，写为回调函数是为了请求接口方便
     gotoIndex = () => ({pathname:'/',query:{token:'12345678'}});
     render() {
+        const token = this.getcookie('token');
+        if(token){
+            return (<div>已登录</div>);
+        }
         return (
             <div>
                 <Link to={this.gotoIndex}>登录</Link>
